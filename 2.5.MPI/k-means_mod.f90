fa18063439760afc9e0fd07aab7a5a619e991b3a
Carlos Barajas <barajasc@umbc.edu>
2018-05-13 01:11:58 -0400

    wrapping basic mpi fortran in python and making notes

---
+++ 2.5.MPI/k-means_mod.f90
@@ -0,0 +1,160 @@
+module kmeans_omp_module
+  !$ use omp_lib
+  implicit none
+  include 'mpif.h'
+
+contains
+  !!! MPI Wrappers
+  !!! MPI INIT which does NOT pass arguments because of how the wrapper works.
+  !!! Comm = MPI_COMM_WORLD to prevent any bad datatype transformations.
+  function mpiInit() result(ierr)
+    integer :: ierr
+    !F2PY INTENT(out) ierr
+    call MPI_INIT(ierr)
+    return 
+  end function mpiInit
+
+  !!! Get rank
+  function mpiGetRank() result(rank)
+    integer :: ierr, rank
+    !F2PY INTENT(out) rank
+    call MPI_COMM_RANK (MPI_COMM_WORLD, rank, ierr)
+    return
+  end function mpiGetRank
+
+  ! Get size
+  function mpiGetSize() result(tprocs)
+    integer :: ierr, tprocs
+    !F2PY INTENT(out) tprocs
+    call MPI_COMM_SIZE(MPI_COMM_WORLD, tprocs, ierr)
+  end function mpiGetSize
+
+  function set_num_threads(nn) result(nt)
+    integer :: nn,nt
+    !F2PY INTENT(IN) :: nn
+    !F2PY INTENT(OUT) :: nt
+    
+    !$ nt = omp_get_max_threads()
+    print*,"Max # of Threads=",nt
+    !$ call omp_set_num_threads(nn)
+    !$ nt = omp_get_max_threads()
+    print*, "Set Max # of Threads=",nt
+  end function set_num_threads
+
+  real(8) function calc_sqdist(aa,bb,nn)
+  !!! Calculate squared sum of distance
+    integer :: nn,ii
+    real(8), dimension(nn),intent(in) :: aa,bb
+    !F2PY INTENT(HIDE) :: nn
+    !F2PY INTENT(OUT) :: calc_sqdist
+
+    calc_sqdist=0.d0
+    do ii=1,nn
+       calc_sqdist=calc_sqdist+(aa(ii)-bb(ii))**2
+    enddo
+
+    return
+  end function calc_sqdist
+
+  real(8) function calc_dist(aa,bb,nn) 
+    integer :: nn,ii
+    real(8),intent(in) :: aa(nn),bb(nn) 
+!    real(8) :: calc_sqdist
+    !F2PY INTENT(HIDE) :: nn
+    !F2PY INTENT(OUT) :: calc_dist
+
+    calc_dist=sqrt(calc_sqdist(aa,bb,nn))
+
+    return
+  end function calc_dist
+
+  subroutine assign_and_get_newsum(indata,ctd,nk,cl,outsum,ncl,nelem,nrec)
+  !!! Calculate sum of data by clusters
+  !!! Need to get new centroid
+  !!! ncl = number of clusters
+  !!! nk = limited number of records to use
+  !!! ii,jj,kk = indexes used during iteration
+  !!! nelem =  number of bins in the histogram
+  !!! nrec = the number of total records in the dataset
+    integer :: nelem,nrec,nk,ncl
+    integer :: ii,jj,kk,idx
+    integer, intent(out) :: cl(nrec)
+    real(8), intent(in) :: indata(nelem,nrec),ctd(nelem,ncl)
+    real(8), intent(out) :: outsum(nelem,ncl)
+    real(8) :: mindd,tmpdd
+    !F2PY INTENT(HIDE) :: ncl,nelem,nrec
+    !F2PY INTENT(OUT) :: cl,outsum
+    !F2PY INTENT(IN) :: nk
+    
+    outsum=0.d0
+    !$OMP PARALLEL DEFAULT(PRIVATE) SHARED(indata,ctd,cl,outsum,ncl,nk,nrec,nelem)
+
+    !!!--- Assigning Clusters
+
+    !!! If OpenMP can split this, so can MPI!
+
+    !$OMP DO
+    !!! From 1->nrec as 1+n*nk
+    do ii=1,nrec,nk
+       !!! Min distance
+       mindd=10000.d0;idx=0
+       do kk=1,ncl
+          tmpdd=calc_sqdist(indata(:,ii), ctd(:,kk), nelem)
+          !!! If calculated distance is less than min distance
+          if (tmpdd.lt.mindd) then
+             mindd=tmpdd; idx=kk
+          endif
+       enddo
+       if (idx.eq.0) then
+          print*,"Not assigned",idx,ii,indata(:,ii)
+          stop
+       endif
+       cl(ii)=idx
+    enddo
+    !$OMP END DO
+    !$OMP BARRIER
+    !!!--- Sum for New Centroid
+
+    !!! This loop order keeps OpenMP thread safe but will require an MPI reduce
+    !!! to merge all of the histograms back into the correct data structure
+    !!! if the loop is parallelized via MPI as well
+
+    !$OMP DO
+    do ii=1,nelem
+       do jj=1,nrec,nk
+          outsum(ii,cl(jj))=outsum(ii,cl(jj))+indata(ii,jj)
+       enddo
+    enddo
+    !$OMP END DO
+    !print*, outsum,cluster(1000:1100)
+    !$OMP END PARALLEL
+
+  end subroutine assign_and_get_newsum
+
+  subroutine get_wcv_sum(indata,ctd,cl,outsum,ncl,nelem,nrec)
+  !!! Calculate sum of data by clusters
+  !!! Need to get new centroid
+    integer :: nelem,nrec,ncl
+    integer :: mm,ii
+    integer, intent(in) :: cl(nrec)
+    real(8), intent(in) :: indata(nelem,nrec),ctd(nelem,ncl)
+    real(8), intent(out) :: outsum(nelem,ncl)
+    !F2PY INTENT(HIDE) :: ncl,nelem,nrec
+    !F2PY INTENT(OUT) :: outsum
+
+    outsum=0.d0
+    !!! Similar story to the previous outsum loop in assign_and_get
+    !!! can be parallelized but will require a Reduce call in order
+    !!! have the correct numbers inside outsum
+    !$OMP PARALLEL DO DEFAULT(PRIVATE) SHARED(outsum,cl,indata,ctd,nrec,nelem)
+    do mm=1,nelem
+       do ii=1,nrec
+          outsum(mm,cl(ii))=outsum(mm,cl(ii))+(indata(mm,ii)-ctd(mm,cl(ii)))**2
+       enddo
+    enddo
+    !$OMP END PARALLEL DO
+
+  end subroutine get_wcv_sum
+
+
+end module kmeans_omp_module
